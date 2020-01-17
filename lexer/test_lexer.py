3909b58269f438060ab72c9f3b1b6a93609bef0d
filipelbc <filipelbc@gmail.com>
2020-01-17 20:27:20 -0300

    Improve test

--- lexer/test_lexer.py
+++ lexer/test_lexer.py
@@ -10,15 +10,26 @@ foo {
   bar
 }
 ---
-(Name('foo'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(Other('{'), Location(None, 1, 4))
-(EndOfLine('\\n'), Location(None, 1, 5))
-(WhiteSpace('  '), Location(None, 1, 0))
-(Name('bar'), Location(None, 1, 2))
-(EndOfLine('\\n'), Location(None, 1, 5))
-(Other('}'), Location(None, 1, 0))
-(EndOfLine('\\n'), Location(None, 1, 1))
+Name('foo')
+WhiteSpace(' ')
+Other('{')
+EndOfLine('\\n')
+WhiteSpace('  ')
+Name('bar')
+EndOfLine('\\n')
+Other('}')
+EndOfLine('\\n')
+"""
+
+CASE_WHITESPACE = """\
+   foo   bar   
+---
+WhiteSpace('   ')
+Name('foo')
+WhiteSpace('   ')
+Name('bar')
+WhiteSpace('   ')
+EndOfLine('\\n')
 """
 
 CASE_COMMENT = """\
@@ -27,53 +38,55 @@ foo {
   bar # another comment
 }
 ---
-(Name('foo'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(Other('{'), Location(None, 1, 4))
-(EndOfLine('\\n'), Location(None, 1, 5))
-(WhiteSpace('  '), Location(None, 1, 0))
-(Comment(None), Location(None, 1, 2))
-(EndOfLine('\\n'), Location(None, 1, 11))
-(WhiteSpace('  '), Location(None, 1, 0))
-(Name('bar'), Location(None, 1, 2))
-(WhiteSpace(' '), Location(None, 1, 5))
-(Comment(None), Location(None, 1, 6))
-(EndOfLine('\\n'), Location(None, 1, 23))
-(Other('}'), Location(None, 1, 0))
-(EndOfLine('\\n'), Location(None, 1, 1))
-"""
-
-CASE_STRING = """\
-foo "bar"
-bar "f\\"o\\"o"
----
-(Name('foo'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(String('bar'), Location(None, 1, 4))
-(EndOfLine('\\n'), Location(None, 1, 9))
-(Name('bar'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(String('f"o"o'), Location(None, 1, 4))
-(EndOfLine('\\n'), Location(None, 1, 13))
+Name('foo')
+WhiteSpace(' ')
+Other('{')
+EndOfLine('\\n')
+WhiteSpace('  ')
+Comment(None)
+EndOfLine('\\n')
+WhiteSpace('  ')
+Name('bar')
+WhiteSpace(' ')
+Comment(None)
+EndOfLine('\\n')
+Other('}')
+EndOfLine('\\n')
 """
 
 CASE_INTEGER = """\
-foo 1
-bar 2
+foo 1 bar 123 bin88
 ---
-(Name('foo'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(PositiveInteger(1), Location(None, 1, 4))
-(EndOfLine('\\n'), Location(None, 1, 5))
-(Name('bar'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(PositiveInteger(2), Location(None, 1, 4))
-(EndOfLine('\\n'), Location(None, 1, 5))
+Name('foo')
+WhiteSpace(' ')
+PositiveInteger(1)
+WhiteSpace(' ')
+Name('bar')
+WhiteSpace(' ')
+PositiveInteger(123)
+WhiteSpace(' ')
+Name('bin88')
+EndOfLine('\\n')
 """
 
+CASE_STRING = """\
+foo "bar" bar "f\\"o\\"o" foo"bar"
+---
+Name('foo')
+WhiteSpace(' ')
+String('bar')
+WhiteSpace(' ')
+Name('bar')
+WhiteSpace(' ')
+String('f"o"o')
+WhiteSpace(' ')
+Name('foo')
+String('bar')
+EndOfLine('\\n')
+"""
 
 # FIXME: enable this case
-CASE_MULTILINE_STRING_1 = """\
+_CASE_MULTILINE_STRING_1 = """\
 foo -8<-
   line 1
   line 2
@@ -86,172 +99,168 @@ CASE_MACRO_BASIC = """\
 macro foo [ bar ]
 ${foo} bin ${foo}
 ---
-(WhiteSpace(' '), Location(None, 1, 0))
-(Name('bar'), Location(None, 1, 1))
-(WhiteSpace(' '), Location(None, 1, 4))
-(WhiteSpace(' '), Location(None, 1, 0))
-(Name('bin'), Location(None, 1, 1))
-(WhiteSpace('  '), Location(None, 1, 4))
-(Name('bar'), Location(None, 1, 6))
-(WhiteSpace(' '), Location(None, 1, 9))
-(EndOfLine('\\n'), Location(None, 1, 0))
+WhiteSpace(' ')
+Name('bar')
+WhiteSpace('  ')
+Name('bin')
+WhiteSpace('  ')
+Name('bar')
+WhiteSpace(' ')
+EndOfLine('\\n')
 """
 
-CASE_MACRO_ARG = """\
+CASE_MACRO_ARG_1 = """\
 macro foo [ bar ${1} ]
-m ${foo "no"} g
+a ${foo "b"} c
 ---
-(Name('m'), Location(None, 1, 0))
-(WhiteSpace('  '), Location(None, 1, 1))
-(Name('bar'), Location(None, 1, 3))
-(WhiteSpace(' '), Location(None, 1, 6))
-(Name('no'), Location(None, 1, 7))
-(WhiteSpace(' '), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 0))
-(Name('g'), Location(None, 1, 1))
-(EndOfLine('\\n'), Location(None, 1, 2))
+Name('a')
+WhiteSpace('  ')
+Name('bar')
+WhiteSpace(' ')
+Name('b')
+WhiteSpace('  ')
+Name('c')
+EndOfLine('\\n')
+"""
+
+CASE_MACRO_ARG_2 = """\
+macro foo [b${1}r]
+a ${foo "a"} c
+---
+Name('a')
+WhiteSpace(' ')
+Name('bar')
+WhiteSpace(' ')
+Name('c')
+EndOfLine('\\n')
+"""
+
+CASE_MACRO_ARG_3 = """\
+macro foo [ ${1}${2} ]
+a ${foo "\\"b" "c\\""} d
+---
+Name('a')
+WhiteSpace('  ')
+String('bc')
+WhiteSpace('  ')
+Name('d')
+EndOfLine('\\n')
 """
 
 CASE_MACRO_MULTILINE_1 = """\
-macro foo [line 1
-  line ${2} line ${1}
+macro foo [
+  b ${2} d
+  e ${1} g
 ]
-bar ${foo
-        "2" "1"}
-"bar"
+a ${foo
+     "f"
+     "c"
+   } h
 ---
-(Name('bar'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(Name('line'), Location(None, 1, 4))
-(WhiteSpace(' '), Location(None, 1, 8))
-(PositiveInteger(1), Location(None, 1, 9))
-(EndOfLine('\\n'), Location(None, 1, 10))
-(WhiteSpace('  '), Location(None, 1, 0))
-(Name('line'), Location(None, 1, 2))
-(WhiteSpace(' '), Location(None, 1, 6))
-(PositiveInteger(1), Location(None, 1, 7))
-(WhiteSpace(' '), Location(None, 1, 0))
-(Name('line'), Location(None, 1, 1))
-(WhiteSpace(' '), Location(None, 1, 5))
-(PositiveInteger(2), Location(None, 1, 6))
-(EndOfLine('\\n'), Location(None, 1, 0))
-(EndOfLine('\\n'), Location(None, 1, 0))
-(String('bar'), Location(None, 1, 0))
-(EndOfLine('\\n'), Location(None, 1, 5))
+Name('a')
+WhiteSpace(' ')
+EndOfLine('\\n')
+WhiteSpace('  ')
+Name('b')
+WhiteSpace(' ')
+Name('c')
+WhiteSpace(' ')
+Name('d')
+EndOfLine('\\n')
+WhiteSpace('  ')
+Name('e')
+WhiteSpace(' ')
+Name('f')
+WhiteSpace(' ')
+Name('g')
+EndOfLine('\\n')
+WhiteSpace(' ')
+Name('h')
+EndOfLine('\\n')
 """
 
 CASE_MACRO_MULTILINE_2 = """\
-macro foo [ line 1
-  line ${2}
-  line ${1}
-]
-bar ${foo
-        "2"
-    "1"
-} "bar"
+macro foo [a
+  b ${2}
+  d
+  e ]
+x ${foo
+    "b"
+    "c"
+} y
 ---
-(Name('bar'), Location(None, 1, 0))
-(WhiteSpace('  '), Location(None, 1, 3))
-(Name('line'), Location(None, 1, 5))
-(WhiteSpace(' '), Location(None, 1, 9))
-(PositiveInteger(1), Location(None, 1, 10))
-(EndOfLine('\\n'), Location(None, 1, 11))
-(WhiteSpace('  '), Location(None, 1, 0))
-(Name('line'), Location(None, 1, 2))
-(WhiteSpace(' '), Location(None, 1, 6))
-(PositiveInteger(1), Location(None, 1, 7))
-(EndOfLine('\\n'), Location(None, 1, 0))
-(WhiteSpace('  '), Location(None, 1, 0))
-(Name('line'), Location(None, 1, 2))
-(WhiteSpace(' '), Location(None, 1, 6))
-(PositiveInteger(2), Location(None, 1, 7))
-(EndOfLine('\\n'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 0))
-(String('bar'), Location(None, 1, 1))
-(EndOfLine('\\n'), Location(None, 1, 6))
+Name('x')
+WhiteSpace(' ')
+Name('a')
+EndOfLine('\\n')
+WhiteSpace('  ')
+Name('b')
+WhiteSpace(' ')
+Name('c')
+EndOfLine('\\n')
+WhiteSpace('  ')
+Name('d')
+EndOfLine('\\n')
+WhiteSpace('  ')
+Name('e')
+WhiteSpace('  ')
+Name('y')
+EndOfLine('\\n')
 """
 
-# FIXME broken: should get Name('fobarin'), Name('bin'), String('babaroo')
 CASE_MACRO_CONCAT_1 = """\
-macro foo [bar]
-fo${foo}in bin "ba${foo}oo"
+macro foo [a]
+b${foo}r x "f${foo}a"
 ---
-(Name('fobar'), Location(None, 1, 0))
-(Name('in'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 2))
-(Name('bin'), Location(None, 1, 3))
-(WhiteSpace(' '), Location(None, 1, 6))
-(Other('"'), Location(None, 1, 7))
-(Name('babar'), Location(None, 1, 8))
-(Name('oo'), Location(None, 1, 0))
-(Other('"'), Location(None, 1, 2))
-(EndOfLine('\\n'), Location(None, 1, 3))
+Name('bar')
+WhiteSpace(' ')
+Name('x')
+WhiteSpace(' ')
+String('faa')
+EndOfLine('\\n')
 """
 
 CASE_MACRO_CONCAT_2 = """\
-macro foo [o
-own line
-b]
-fo${foo}in bin fo${foo}in
+macro foo [a
+b
+o]
+b${foo}r x f${foo}r
 ---
-(Name('foo'), Location(None, 1, 0))
-(EndOfLine('\\n'), Location(None, 1, 3))
-(Name('own'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(Name('line'), Location(None, 1, 4))
-(EndOfLine('\\n'), Location(None, 1, 8))
-(Name('b'), Location(None, 1, 0))
-(Name('in'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 2))
-(Name('bin'), Location(None, 1, 3))
-(WhiteSpace(' '), Location(None, 1, 6))
-(Name('foo'), Location(None, 1, 7))
-(EndOfLine('\\n'), Location(None, 1, 10))
-(Name('own'), Location(None, 1, 0))
-(WhiteSpace(' '), Location(None, 1, 3))
-(Name('line'), Location(None, 1, 4))
-(EndOfLine('\\n'), Location(None, 1, 8))
-(Name('b'), Location(None, 1, 0))
-(Name('in'), Location(None, 1, 0))
-(EndOfLine('\\n'), Location(None, 1, 2))
+Name('ba')
+EndOfLine('\\n')
+Name('b')
+EndOfLine('\\n')
+Name('or')
+WhiteSpace(' ')
+Name('x')
+WhiteSpace(' ')
+Name('fa')
+EndOfLine('\\n')
+Name('b')
+EndOfLine('\\n')
+Name('or')
+EndOfLine('\\n')
 """
 
 
+def with_cases(cls):
+    g = globals()
+    for c in g:
+        if c.startswith('CASE_'):
+
+            def test_case(self, case=g[c]):
+                self.check(case)
+
+            setattr(cls, 'test_' + c[5:].lower(), test_case)
+    return cls
+
+
+@with_cases
 class Test(TestCase):
 
     maxDiff = None
 
     def check(self, case):
         source, expected_tokens = case.split('---\n')
-        tokens = '\n'.join(str(x) for x in UntilNoneIterator(Lexer(source)))
+        tokens = '\n'.join(str(x[0]) for x in UntilNoneIterator(Lexer(source)))
         self.assertEqual(expected_tokens, tokens + '\n')
-
-    def test_basic(self):
-        self.check(CASE_BASIC)
-
-    def test_comment(self):
-        self.check(CASE_COMMENT)
-
-    def test_string(self):
-        self.check(CASE_STRING)
-
-    def test_integer(self):
-        self.check(CASE_INTEGER)
-
-    def test_macro_basic(self):
-        self.check(CASE_MACRO_BASIC)
-
-    def test_macro_arg(self):
-        self.check(CASE_MACRO_ARG)
-
-    def test_macro_multiline_1(self):
-        self.check(CASE_MACRO_MULTILINE_1)
-
-    def test_macro_multiline_2(self):
-        self.check(CASE_MACRO_MULTILINE_2)
-
-    def test_macro_concat_1(self):
-        self.check(CASE_MACRO_CONCAT_1)
-
-    def test_macro_concat_2(self):
-        self.check(CASE_MACRO_CONCAT_2)
