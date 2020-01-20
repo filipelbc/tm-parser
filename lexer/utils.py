e749cfe085388ae6663e359007cbfd9e81abd0e5
filipelbc <filipelbc@gmail.com>
2020-01-19 21:30:08 -0300

    Add BufferedStream to utils

--- lexer/utils.py
+++ lexer/utils.py
@@ -1,7 +1,7 @@
 class UntilNoneIterator:
     """
-    Generic iteretor for objects whose __next__ returns None instead of raising
-    StopIteration.
+    Generic iteretor for objects whose `__next__` returns None instead of raising
+    `StopIteration`.
 
     >>> class Foo:
     ...     x = 4
@@ -31,3 +31,51 @@ class UntilNoneIterator:
 
     def __iter__(self):
         return self
+
+
+class BufferedStream:
+    """
+    Wraps a stream object, providing a way to give back an item to the stream.
+    Useful for adding look-ahead functionality to an iterator.
+
+    >>> s = BufferedStream(iter(range(6)))
+    >>> next(s)
+    0
+    >>> i = next(s); i
+    1
+    >>> s.take_back(i)
+    >>> next(s)
+    1
+    >>> next(s)
+    2
+    >>> s.peek()
+    3
+    >>> s.peek()
+    3
+    >>> i = next(s); i
+    3
+    >>> j = next(s); j
+    4
+    >>> s.take_back(j); s.take_back(i)
+    >>> next(s)
+    3
+    >>> next(s)
+    4
+    """
+
+    def __init__(self, stream):
+        self.buffer = []
+        self.stream = stream
+
+    def __next__(self):
+        if self.buffer:
+            return self.buffer.pop()
+        return next(self.stream)
+
+    def take_back(self, obj):
+        self.buffer.append(obj)
+
+    def peek(self):
+        obj = next(self)
+        self.take_back(obj)
+        return obj
